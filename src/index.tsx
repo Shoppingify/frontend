a0eacffdd5314132e11b61667edddfc2963cf466
afajfar <anze.fajfar@agiledrop.com>
2020-08-28 23:36:34 +0200

    Refactor folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
 import App from './App';
-import "./styles.css";
-import "./styles.scss";
+import "./styles/styles.css";
+import "./styles/styles.scss";
 
-const mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Shoppingify" />, mountNode);
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
