aa53c53738697a884e4a3810c46e6dff917adb9a
afajfar <anze.fajfar@agiledrop.com>
2020-08-29 21:55:42 +0200

    Feat: Add app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
 import { hot } from "react-hot-loader/root";
-import React from "react";
+import React, { useEffect } from "react";
 
-interface Props {
-   name: string
-}
+// Libs
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App(props: Props) {
+// Router components
+import RoutesController from "./routes/RoutesController";
 
-  const { name } = props;
+// Components
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import { useRecoilState, atom } from "recoil/dist";
+
+// Recoil atom
+export const userState = atom({
+  key: 'userState',
+  default: {
+    token: ''
+  }
+})
+
+function App() {
+  // Recoil user state
+  const [user, setUser] = useRecoilState(userState);
+
+  /**
+   * On mount effect
+   */
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+      // @ts-ignore
+      setUser((current: object) => (
+        {
+          ...current,
+          token: localStorage.getItem('token')
+        }
+      ))
+    }
+  }, [])
 
   return (
-    <>
-      <h1 className="text-4xl text-white bg-black">
-        Hello {name}
-      </h1>
-    </>
+    <Router>
+      <div className="flex justify-between h-screen">
+        { user.token && <Navbar /> }
+        <div className="flex-grow">
+          <RoutesController />
+        </div>
+        { user.token && <Sidebar /> }
+      </div>
+    </Router>
   );
 }
 
