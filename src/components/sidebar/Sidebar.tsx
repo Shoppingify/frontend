5a0a204f42c1f82f361226d58be12775f7750e5c
afajfar <anze.fajfar@agiledrop.com>
2020-08-29 21:54:15 +0200

    Feat: Simple sidebar

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="w-1/3">
+      <h1 className="text-2xl">Sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
