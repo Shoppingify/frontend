01f484f1a82277e390c4a7ce3358e24b4501729f
fajfara <a.fajfar95@gmail.com>
2020-08-31 23:02:34 +0200

    FEAT: Add colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,20 @@ module.exports = {
             './src/**/*.tsx',
         ],
     },
+    theme: {
+        colors: {
+            primary: '#F9A109',
+            "primary-light": '#FFF0DE',
+            secondary: '#56CCF2',
+            danger: '#EB5757',
+            white: '#fff',
+            black: '#000000',
+            gray: '#454545',
+            "gray-light": '#c1c1c4',
+            "gray-extra-light": '#FAFAFE'
+        },
+        fontFamily: {
+            body: ['Quicksand', 'sans-serif'],
+        },
+    }
 }
