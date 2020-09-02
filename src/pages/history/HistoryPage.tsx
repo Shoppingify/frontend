7ff19812a9ee6e1756dacb7727b2d8e21048ad65
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-02 14:47:38 +0200

    CONFIG: Add prettier + eslint + change function to arrow function in History Page and cards/List component

--- src/pages/history/HistoryPage.tsx
+++ src/pages/history/HistoryPage.tsx
@@ -1,95 +1,95 @@
-import React, { useState, useEffect, useCallback } from "react"
-import { format } from "date-fns"
+import React, { useState, useEffect, useCallback } from 'react'
+import { format } from 'date-fns'
 
-import { useRecoilValue } from "recoil"
-import { userState } from "../../App"
-import List from "../../components/cards/List"
+import { useRecoilValue } from 'recoil'
+import { userState } from '../../App'
+import List from '../../components/cards/List'
 
 /**
  * Simple history page component
  */
-function HistoryPage() {
-  const [lists, setLists] = useState([])
-  const [loading, setLoading] = useState(true)
-  const user: any = useRecoilValue(userState)
+const HistoryPage = () => {
+    const [lists, setLists] = useState([])
+    const [loading, setLoading] = useState(true)
+    const user: any = useRecoilValue(userState)
 
-  /**
-   * Fetch the user's lists
-   */
-  const fetchLists = useCallback(async () => {
-    try {
-      const headers = new Headers()
-      headers.append("Authorization", `Bearer ${user.token}`)
+    /**
+     * Fetch the user's lists
+     */
+    const fetchLists = useCallback(async () => {
+        try {
+            const headers = new Headers()
+            headers.append('Authorization', `Bearer ${user.token}`)
 
-      const response = await fetch("http://localhost:3000/api/lists", {
-        method: "GET",
-        headers,
-      })
-      const { data } = await response.json()
+            const response = await fetch('http://localhost:3000/api/lists', {
+                method: 'GET',
+                headers,
+            })
+            const { data } = await response.json()
 
-      // Group data by date
-      const sorted = groupByDate(data)
+            // Group data by date
+            const sorted = groupByDate(data)
 
-      setLists(() => sorted)
-    } catch (e) {
-      console.log("error", e)
-    } finally {
-      setLoading(false)
-    }
-  }, [])
-
-  /**
-   * Group the lists by date
-   * @param {List[]} data:
-   */
-  const groupByDate = (data: any) => {
-    return data.reduce((acc: any, value: any) => {
-      const date = format(new Date(value.created_at), "MMMM yyyy")
-
-      if (acc.length === 0) {
-        acc.push({
-          date,
-          lists: [].concat(value),
-        })
-      } else {
-        const index: number = acc.findIndex((i: any) => i.date === date)
-        if (index !== -1) {
-          acc[index].lists.push(value)
-        } else {
-          acc.push({
-            date,
-            lists: [].concat(value),
-          })
+            setLists(() => sorted)
+        } catch (e) {
+            console.log('error', e)
+        } finally {
+            setLoading(false)
         }
-      }
-      return acc
     }, [])
-  }
 
-  useEffect(() => {
-    fetchLists()
-  }, [])
+    /**
+     * Group the lists by date
+     * @param {List[]} data:
+     */
+    const groupByDate = (data: any) => {
+        return data.reduce((acc: any, value: any) => {
+            const date = format(new Date(value.created_at), 'MMMM yyyy')
 
-  return (
-    <div className="px-10 bg-gray-extra-light h-full">
-      <h1 className="text-2xl font-bold pt-8">Shopping History</h1>
+            if (acc.length === 0) {
+                acc.push({
+                    date,
+                    lists: [].concat(value),
+                })
+            } else {
+                const index: number = acc.findIndex((i: any) => i.date === date)
+                if (index !== -1) {
+                    acc[index].lists.push(value)
+                } else {
+                    acc.push({
+                        date,
+                        lists: [].concat(value),
+                    })
+                }
+            }
+            return acc
+        }, [])
+    }
 
-      {/* TODO: Make a proper loader */}
-      {loading && <div>Loading...</div>}
+    useEffect(() => {
+        fetchLists()
+    }, [])
 
-      {/* Lists */}
-      {lists.map((item: any) => (
-        <div className="mt-10" key={item.date}>
-          <h3 className="text-sm mb-4 font-medium">{item.date}</h3>
-          <ul>
-            {item.lists.map((list: any) => (
-              <List key={list.id} list={list} />
+    return (
+        <div className="px-10 bg-gray-extra-light h-full">
+            <h1 className="text-2xl font-bold pt-8">Shopping History</h1>
+
+            {/* TODO: Make a proper loader */}
+            {loading && <div>Loading...</div>}
+
+            {/* Lists */}
+            {lists.map((item: any) => (
+                <div className="mt-10" key={item.date}>
+                    <h3 className="text-sm mb-4 font-medium">{item.date}</h3>
+                    <ul>
+                        {item.lists.map((list: any) => (
+                            <List key={list.id} list={list} />
+                        ))}
+                    </ul>
+                </div>
             ))}
-          </ul>
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default HistoryPage
