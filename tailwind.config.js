fc7b3af93e74f210e64447631998390e1d0f020c
ips-coding-challenge <67691276+ips-coding-challenge@users.noreply.github.com>
2020-09-02 12:02:43 +0200

    FEAT: History Page (#7)
    * FEAT: Begin to work on  history page
    
    * FEAT: Add types for props and add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 module.exports = {
-    purge: {
-        enabled: false,
-        content: [
-            './src/**/*.jsx',
-            './src/**/*.tsx',
-        ],
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.jsx", "./src/**/*.tsx"],
+  },
+  theme: {
+    colors: {
+      primary: "#F9A109",
+      "primary-light": "#FFF0DE",
+      secondary: "#56CCF2",
+      danger: "#EB5757",
+      white: "#fff",
+      black: "#000000",
+      gray: "#454545",
+      "gray-light": "#c1c1c4",
+      "gray-extra-light": "#FAFAFE",
     },
-    theme: {
-        colors: {
-            primary: '#F9A109',
-            "primary-light": '#FFF0DE',
-            secondary: '#56CCF2',
-            danger: '#EB5757',
-            white: '#fff',
-            black: '#000000',
-            gray: '#454545',
-            "gray-light": '#c1c1c4',
-            "gray-extra-light": '#FAFAFE'
-        },
-        fontFamily: {
-            body: ['Quicksand', 'sans-serif'],
-        },
-    }
-}
+    fontFamily: {
+      body: ["Quicksand", "sans-serif"],
+    },
+  },
+};
