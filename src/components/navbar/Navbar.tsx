170607cf136ce3130aa3bacff1c1fde8af809031
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-09 13:45:02 +0200

    FEAT: Style the Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 
 // Libs
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom'
+import {
+    AiOutlineUnorderedList,
+    AiOutlineReload,
+    AiOutlineShoppingCart,
+} from 'react-icons/ai'
+import { IoMdStats } from 'react-icons/io'
+import logo from '../../assets/logo.svg'
 /**
  * Simple navbar component
  */
 // TODO refactor
-function Navbar() {
-  return (
-    <nav className="w-24">
-      <ul>
-        <li>
-          <Link to="/items">Items</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
-        <li>
-          <Link to="/statistics">Statistics</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+const Navbar = () => {
+    const location = useLocation()
+
+    useEffect(() => {
+        console.log('location', location)
+    }, [location])
+
+    return (
+        <nav className="w-24 flex-none flex flex-col items-center mt-10">
+            <img src={logo} alt="logo" />
+            <ul className="flex flex-col w-full h-full justify-center items-center">
+                <li
+                    className={`navbar__item ${
+                        location && location.pathname === '/items'
+                            ? 'active'
+                            : ''
+                    }`}
+                >
+                    <Link to="/items">
+                        <AiOutlineUnorderedList />
+                    </Link>
+                </li>
+                <li
+                    className={`navbar__item ${
+                        location && location.pathname === '/history'
+                            ? 'active'
+                            : ''
+                    }`}
+                >
+                    <Link to="/history">
+                        <AiOutlineReload />
+                    </Link>
+                </li>
+                <li
+                    className={`navbar__item ${
+                        location && location.pathname === '/statistics'
+                            ? 'active'
+                            : ''
+                    }`}
+                >
+                    <Link to="/statistics">
+                        <IoMdStats />
+                    </Link>
+                </li>
+            </ul>
+            <div className="w-16 h-16 flex justify-center items-center relative">
+                <div
+                    style={{ top: '4px', right: '4px' }}
+                    className="absolute w-5 h-5 bg-danger rounded-lg text-white flex justify-center items-center text-sm "
+                >
+                    3
+                </div>
+                <div className="rounded-full flex justify-center items-center bg-primary w-10 h-10  text-white text-xl">
+                    <AiOutlineShoppingCart />
+                </div>
+            </div>
+        </nav>
+    )
 }
 
-export default Navbar;
+export default Navbar
