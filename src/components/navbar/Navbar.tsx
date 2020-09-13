83a83dd6050aefb3e1af97f27efd76b7cb663237
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-13 16:56:50 +0200

    FEAT: add the tooltip to the navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { MdInsertChart, MdList, MdRefresh } from 'react-icons/md'
 // Libs
 import { NavLink } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import ShoppingBasket from '../shopping-basket/ShoppingBasket'
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css' // optional for styling
 
 /**
  * Simple navbar component
  */
 // TODO refactor
 const Navbar = () => {
+    useEffect(() => {
+        tippy('[data-tippy-content]', {
+            placement: 'right',
+            offset: [0, -10],
+        })
+    }, [])
     return (
         <nav className="w-24 flex-none flex flex-col items-center mt-10">
             <img src={logo} alt="logo" />
             <ul className="flex flex-col w-full h-full justify-center items-center">
                 <li className="w-full">
                     <NavLink
+                        data-tippy-content="items"
                         className="navbar__item"
                         to="/items"
                         activeClassName="active"
@@ -25,6 +34,7 @@ const Navbar = () => {
                 </li>
                 <li className="w-full">
                     <NavLink
+                        data-tippy-content="history"
                         className="navbar__item"
                         to="/history"
                         activeClassName="active"
@@ -34,6 +44,7 @@ const Navbar = () => {
                 </li>
                 <li className="w-full">
                     <NavLink
+                        data-tippy-content="statistics"
                         className="navbar__item"
                         to="/statistics"
                         activeClassName="active"
