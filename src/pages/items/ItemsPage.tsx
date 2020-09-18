c4ceee8006b52bc42ca0d215cd08a7db059ae083
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-18 14:01:50 +0200

    FEAT: Handle item not found while searching

--- src/pages/items/ItemsPage.tsx
+++ src/pages/items/ItemsPage.tsx
@@ -167,49 +167,58 @@ const ItemsPage: React.FC = () => {
                 </h1>
                 <SearchInput search={searchItems} />
             </div>
-            <motion.ul
-                variants={containerVariants}
-                initial="hidden"
-                animate="show"
-                className="overflow-y-auto px-3 md:px-5 lg:px-10"
-            >
-                {filteredItems.map((listOfItems: ItemsWithCategories) => (
-                    <li key={uuidv4()} className="mb-10">
-                        {/* Category name component */}
-                        <CategoryTitle
-                            category={listOfItems.category}
-                            category_id={listOfItems.category_id}
-                            categoryUpdated={categoryUpdated}
-                        />
-                        <ul className="grid grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-6 w-full">
-                            {listOfItems.items.length > 0 &&
-                                listOfItems.items.map((item: ItemType) => (
-                                    <motion.li
-                                        variants={itemVariants}
-                                        key={uuidv4()}
-                                    >
-                                        <Item
-                                            data={item}
-                                            category={listOfItems.category}
-                                        />
-                                    </motion.li>
-                                ))}
 
-                            {listOfItems.items.length === 0 && (
-                                <Button
-                                    modifier=""
-                                    onClick={() =>
-                                        addAnItem(listOfItems.category)
-                                    }
-                                    className="text-gray"
-                                >
-                                    Add an item
-                                </Button>
-                            )}
-                        </ul>
-                    </li>
-                ))}
-            </motion.ul>
+            {filteredItems.length === 0 && (
+                <div className="flex justify-center">
+                    <h3 className="text-xl font-bold">No item found...</h3>
+                </div>
+            )}
+
+            {filteredItems.length > 0 && (
+                <motion.ul
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="show"
+                    className="overflow-y-auto px-3 md:px-5 lg:px-10"
+                >
+                    {filteredItems.map((listOfItems: ItemsWithCategories) => (
+                        <li key={uuidv4()} className="mb-10">
+                            {/* Category name component */}
+                            <CategoryTitle
+                                category={listOfItems.category}
+                                category_id={listOfItems.category_id}
+                                categoryUpdated={categoryUpdated}
+                            />
+                            <ul className="grid grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-6 w-full">
+                                {listOfItems.items.length > 0 &&
+                                    listOfItems.items.map((item: ItemType) => (
+                                        <motion.li
+                                            variants={itemVariants}
+                                            key={uuidv4()}
+                                        >
+                                            <Item
+                                                data={item}
+                                                category={listOfItems.category}
+                                            />
+                                        </motion.li>
+                                    ))}
+
+                                {listOfItems.items.length === 0 && (
+                                    <Button
+                                        modifier=""
+                                        onClick={() =>
+                                            addAnItem(listOfItems.category)
+                                        }
+                                        className="text-gray"
+                                    >
+                                        Add an item
+                                    </Button>
+                                )}
+                            </ul>
+                        </li>
+                    ))}
+                </motion.ul>
+            )}
         </div>
     )
 }
