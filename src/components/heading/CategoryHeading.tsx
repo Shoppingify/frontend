93998e75da6785f439b8a2df3f1f1aada3a434c7
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-22 15:25:31 +0200

    Small CategoryHeading refactor

--- src/components/heading/CategoryHeading.tsx
+++ src/components/heading/CategoryHeading.tsx
@@ -3,10 +3,8 @@ import { useRecoilValue } from 'recoil'
 import { singleCategoryState } from '../../global-state/categoriesState'
 import Heading from './Heading'
 
-const CategoryHeading = (props: any) => {
-    const { category_id, ...other } = props
+const CategoryHeading = ({ category_id, ...other }: any) => {
     const singleCategory = useRecoilValue(singleCategoryState(category_id))
-    console.log('headingProps', other)
     return <Heading {...other}>{singleCategory.name}</Heading>
 }
 
