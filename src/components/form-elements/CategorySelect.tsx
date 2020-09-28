8a08e7a12f4082808431113e0c229f9e7934ace3
ips-coding-challenge <coding_challenge@laposte.net>
2020-09-28 14:25:32 +0200

    FIX: add null verification on category to avoid error when adding a new item

--- src/components/form-elements/CategorySelect.tsx
+++ src/components/form-elements/CategorySelect.tsx
@@ -49,23 +49,22 @@ const CategorySelect = ({ label, ...props }: any) => {
 
     useEffect(() => {
         let catNameUpToDate
+
         if (currentItem?.category_id) {
             const category = categories.find((cat: any) => {
                 return cat.id === currentItem?.category_id
             })
-            catNameUpToDate = category.name
+            catNameUpToDate = category?.name
         } else {
             // It's a new item on an empty category so I need to always
             // check if the category name has been updated too
             const category = categories.find((cat: any) => {
-                console.log('cat.name', cat.name)
-                console.log('currentItem cat name', currentItem?.categoryName)
                 return (
                     cat.name.toLowerCase() ===
                     currentItem?.categoryName?.toLowerCase()
                 )
             })
-            catNameUpToDate = category.name
+            catNameUpToDate = category?.name
         }
 
         helpers.setValue(catNameUpToDate || '', true)
