98258092fc4a69310dd688af53fe64ff24bc2d00
ips-coding-challenge <coding_challenge@laposte.net>
2020-10-13 18:12:52 +0200

    FIX: Change stats categories color according to design (#165)

--- src/components/stats/Progress.tsx
+++ src/components/stats/Progress.tsx
@@ -3,21 +3,17 @@ import React from 'react'
 
 interface ProgressProps {
     value: number
+    colorClass: string
 }
 
-const variants = {
-    initial: {
-        width: 0,
-    },
-}
-const Progress = ({ value }: ProgressProps) => {
+const Progress = ({ value, colorClass }: ProgressProps) => {
     return (
         <div className="relative w-full h-2 rounded-12 bg-gray-progress">
             <motion.div
                 style={{ width: `0` }}
                 animate={{ width: `${value}%` }}
                 transition={{ duration: 1 }}
-                className="absolute left-0 top-0 bottom-0 rounded-12 bg-primary"
+                className={`absolute left-0 top-0 bottom-0 rounded-12 ${colorClass}`}
             ></motion.div>
         </div>
     )
